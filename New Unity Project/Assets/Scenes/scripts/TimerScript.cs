7bd77a9597e98edb11e598bbbe0c3804eb74e00b
Sergey <S1xDevelopN@gmail.com>
2019-07-09 09:13:40 +0300

    1

---
+++ New Unity Project/Assets/Scenes/scripts/TimerScript.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimerScript : MonoBehaviour
+{
+
+    public GameObject Object;
+    public float timer1;
+
+    void Start()
+    {
+
+    }
+
+
+    void Update()
+    {
+
+    }
+}
---
+++ New Unity Project/Assets/Scenes/scripts/endGame.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class endGame : MonoBehaviour
+{
+
+    public GameObject Pl;
+
+   
+
+    public bool isEnded;
+    public GameObject EndMenu;
+
+    void Start()
+    {
+        EndMenu.SetActive(false);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        
+        if(col.gameObject == Pl)
+        {
+            EndMenu.SetActive(true);
+        }
+    }
+}
